827c9ff555b7e2761572ccdd242ffe79b518554f
Olivier Bitter <bittero@student.ethz.ch>
2018-06-22 13:43:22 +0200

    Finish assist scraping

--- assist_score_scraper.py
+++ assist_score_scraper.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import unicodedata
 
+def create_json_entry(name, assists):
+    return '{"name":"' + name + '", "assists": "' + assists +'"}'
+
 
 headers = {'User-Agent': 
            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
@@ -15,8 +18,19 @@ pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
 
 Values = pageSoup.find_all("td", {"class": ["hell", "dunkel"]})
 
+json_string = '['
+
 for i in range(len(Values) // 6):
     player = Values[6*i+1].text
     player = unicodedata.normalize('NFKD', player).encode('ascii','ignore').decode("utf-8").strip()
     assists = Values[6*i+5].text
-    print(player + ", " + assists)
+    json_string += create_json_entry(player, assists) + ','
+
+json_string = json_string[:-1] + ']'
+print(json_string)
+
+assistsFile = open("assists.json","w")
+assistsFile.write(json_string)
+assistsFile.close()
+
+
